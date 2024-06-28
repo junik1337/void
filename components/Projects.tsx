e35c7d175924547d42a5b17a6f081bd06168c7c2
junik1337 <abderrachidyassir@gmail.com>
2024-06-28 15:07:16 +0100

    Add: footer is done i think lol

---
+++ components/Projects.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+	return (
+		<section
+			id='projects'
+			className='w-screen h-screen'
+		>
+			
+		</section>
+	);
+};
+
+export default Projects;
